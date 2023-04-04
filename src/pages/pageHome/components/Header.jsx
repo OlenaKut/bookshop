493cb7edc1c36c5b948f3415e7f96f231250bdcb
OlenaKut <105447315+OlenaKut@users.noreply.github.com>
2023-04-04 12:09:32 +0200

    Added shoping cart and login button

--- src/pages/pageHome/components/Header.jsx
+++ src/pages/pageHome/components/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from 'react-bootstrap/Button';
 
 const Header = () => {
   return (
@@ -10,7 +11,12 @@ const Header = () => {
         <Col>
           <h1 className="logo">BookShop</h1>
           <p className="logo-under-text">your best friend</p>
+        </Col> 
+        <Col className="">
+          <p>Welcome,</p>
+          <Button variant="link" href="#" target="_blank" rel="noopener noreferrer">Guest</Button>
         </Col>
+        <Col><p>Shopping Cart(), <Button variant="link" href="#" target="_blank" rel="noopener noreferrer">$0.00</Button></p></Col>
       </Row>
     </Container>
   );
