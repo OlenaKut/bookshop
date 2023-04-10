437a1519b858968d83fda0a714cf2cfee6b1474c
JuliaSmolianska <i.julia.i1990@gmail.com>
2023-04-10 20:03:15 +0200

    change Subheader

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import Container from "react-bootstrap/Container";
 import HomePage from "./pages/pageHome/HomePage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import Subheader from "./pages/pageHome/components/Subheader";
 
 const App = () => {
   return (
     <Container fluid className="App">
       <HomePage />
-      <Subheader />
     </Container>
   );
 };
--- src/pages/pageHome/HomePage.js
+++ src/pages/pageHome/HomePage.js
@@ -2,11 +2,13 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Header from "./components/Header";
 import "./HomePage.css";
+import Subheader from "./components/Subheader";
 
 const HomePage = () => {
   return (
     <Container className="m-0 p-0">
       <Header />
+      <Subheader />
     </Container>
   );
 };
