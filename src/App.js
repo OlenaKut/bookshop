51fc6918c4afbeeb4571ef19676758222a0d3a6d
JuliaSmolianska <i.julia.i1990@gmail.com>
2023-04-10 19:46:34 +0200

    add Subheader

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import Container from "react-bootstrap/Container";
 import HomePage from "./pages/pageHome/HomePage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Subheader from "./pages/pageHome/components/Subheader";
 
 const App = () => {
   return (
     <Container fluid className="App">
       <HomePage />
+      <Subheader />
     </Container>
   );
 };
