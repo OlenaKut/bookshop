06186c719142238645e95b5bd065614f8e884785
OlenaKut <cundrochka@gmail.com>
2023-04-12 00:20:05 +0200

    Added card one

--- src/pages/pageHome/components/Main.jsx
+++ src/pages/pageHome/components/Main.jsx
@@ -3,162 +3,157 @@ import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import "./Main.css";
 
 const Main = () => {
   return (
     <div className="m-auto">
       <Container fluid className="m-auto text-center my-5">
         <Row className="d-inline-flex justify-content-around" xs="auto">
+          <Card
+            className="card-main p-0 d-flex mb-3"
+            style={{ width: "18rem" }}
+          >
+            <Card.Img
+              variant="top"
+              src="images/books/1.webp"
+              className="rounded w-75 m-auto"
+            />
+            <Card.Body className="text-start p-3">
+              <Card.Title className="card-title">
+                You Could Make This Place Beautiful: A Memoir
+                <br />
+                <span className="card-author fs-6 fst-italic">
+                  by Maggie Smith
+                </span>
+              </Card.Title>
+              <Card.Text>
+                "Life, like a poem, is a series of choices." In her memoir You
+                Could Make This Place Beautiful, poet Maggie Smith explores the
+                disintegration of her marriage and her renewed commitment to
+                herself in lyrical vignettes that shine, hard and clear as
+                jewels.
+              </Card.Text>
+            </Card.Body>
+
+            <ButtonGroup aria-label="Basic example">
+              <Button variant="link" className="buy-button fw-bold">
+                BUY NOW
+              </Button>
+              <Button variant="link" className="price-button fw-bold">
+                $22
+              </Button>
+            </ButtonGroup>
+          </Card>
           <Col>
             <Card
-              className="card-main mb-3"
-              style={{ width: "18rem", height: "18rem" }}
+              className="card-main p-0 d-flex mb-3"
+              style={{ width: "18rem" }}
             >
-              <Card.Img variant="top" src="holder.js/100px160" />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
+              <Card.Img
+                variant="top"
+                src="images/books/1.webp"
+                className="rounded w-75 m-auto"
+              />
+              <Card.Body className="text-start p-3">
+                <Card.Title className="card-title">
+                  You Could Make This Place Beautiful: A Memoir
+                  <br />
+                  <span className="card-author fs-6 fst-italic">
+                    by Maggie Smith
+                  </span>
+                </Card.Title>
                 <Card.Text>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
+                  "Life, like a poem, is a series of choices." In her memoir You
+                  Could Make This Place Beautiful, poet Maggie Smith explores
+                  the disintegration of her marriage and her renewed commitment
+                  to herself in lyrical vignettes that shine, hard and clear as
+                  jewels.
                 </Card.Text>
               </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Footer>
+
+              <ButtonGroup aria-label="Basic example">
+                <Button variant="link" className="buy-button fw-bold">
+                  BUY NOW
+                </Button>
+                <Button variant="link" className="price-button fw-bold">
+                  $22
+                </Button>
+              </ButtonGroup>
             </Card>
           </Col>
           <Col>
             <Card
-              className="card-main mb-3"
-              style={{ width: "18rem", height: "18rem" }}
+              className="card-main p-0 d-flex mb-3"
+              style={{ width: "18rem" }}
             >
-              <Card.Img variant="top" src="holder.js/100px160" />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
+              <Card.Img
+                variant="top"
+                src="images/books/1.webp"
+                className="rounded w-75 m-auto"
+              />
+              <Card.Body className="text-start p-3">
+                <Card.Title className="card-title">
+                  You Could Make This Place Beautiful: A Memoir
+                  <br />
+                  <span className="card-author fs-6 fst-italic">
+                    by Maggie Smith
+                  </span>
+                </Card.Title>
                 <Card.Text>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
+                  "Life, like a poem, is a series of choices." In her memoir You
+                  Could Make This Place Beautiful, poet Maggie Smith explores
+                  the disintegration of her marriage and her renewed commitment
+                  to herself in lyrical vignettes that shine, hard and clear as
+                  jewels.
                 </Card.Text>
               </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Footer>
+              <ButtonGroup aria-label="Basic example">
+                <Button variant="link" className="buy-button fw-bold">
+                  BUY NOW
+                </Button>
+                <Button variant="link" className="price-button fw-bold">
+                  $22
+                </Button>
+              </ButtonGroup>
             </Card>
           </Col>
           <Col>
             <Card
-              className="card-main mb-3"
-              style={{ width: "18rem", height: "18rem" }}
+              className="card-main p-0 d-flex mb-3 d-lg-none d-xl-none d-xxl-flex"
+              style={{ width: "18rem" }}
             >
-              <Card.Img variant="top" src="holder.js/100px160" />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
+              <Card.Img
+                variant="top"
+                src="images/books/1.webp"
+                className="rounded w-75 m-auto"
+              />
+              <Card.Body className="text-start p-3">
+                <Card.Title className="card-title">
+                  You Could Make This Place Beautiful: A Memoir
+                  <br />
+                  <span className="card-author fs-6 fst-italic">
+                    by Maggie Smith
+                  </span>
+                </Card.Title>
                 <Card.Text>
-                  This card has supporting text below as a natural lead-in to
-                  additional content.{" "}
+                  "Life, like a poem, is a series of choices." In her memoir You
+                  Could Make This Place Beautiful, poet Maggie Smith explores
+                  the disintegration of her marriage and her renewed commitment
+                  to herself in lyrical vignettes that shine, hard and clear as
+                  jewels.
                 </Card.Text>
               </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Footer>
-            </Card>
-          </Col>
-          <Col>
-            <Card
-              className="card-main mb-3 d-flex d-lg-none d-xl-none d-xxl-flex"
-              style={{ width: "18rem", height: "18rem" }}
-            >
-              <Card.Img variant="top" src="holder.js/100px160" />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This card has even longer
-                  content than the first to show that equal height action.
-                </Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Footer>
-            </Card>
-          </Col>
-        </Row>
-        <Row className="d-inline-flex justify-content-around" xs="auto">
-          <Col>
-            <Card
-              className="card-main mb-3"
-              style={{ width: "18rem", height: "18rem" }}
-            >
-              <Card.Img variant="top" src="holder.js/100px160" />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Footer>
-            </Card>
-          </Col>
-          <Col>
-            <Card
-              className="card-main mb-3"
-              style={{ width: "18rem", height: "18rem" }}
-            >
-              <Card.Img variant="top" src="holder.js/100px160" />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Footer>
-            </Card>
-          </Col>
-          <Col>
-            <Card
-              className="card-main mb-3"
-              style={{ width: "18rem", height: "18rem" }}
-            >
-              <Card.Img variant="top" src="holder.js/100px160" />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This card has supporting text below as a natural lead-in to
-                  additional content.{" "}
-                </Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Footer>
-            </Card>
-          </Col>
-          <Col>
-            <Card
-              className="card-main mb-3 d-flex d-lg-none d-xl-none d-xxl-flex"
-              style={{ width: "18rem", height: "18rem" }}
-            >
-              <Card.Img variant="top" src="holder.js/100px160" />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This card has even longer
-                  content than the first to show that equal height action.
-                </Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Footer>
+              <ButtonGroup aria-label="Basic example">
+                <Button variant="link" className="buy-button fw-bold">
+                  BUY NOW
+                </Button>
+                <Button variant="link" className="price-button fw-bold">
+                  $22
+                </Button>
+              </ButtonGroup>
             </Card>
           </Col>
         </Row>
