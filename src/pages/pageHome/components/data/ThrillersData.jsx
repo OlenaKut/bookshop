583b6678a32324d13941310cf4e6e7c23e819998
OlenaKut <cundrochka@gmail.com>
2023-04-14 11:53:18 +0200

    Added data thrillers

---
+++ src/pages/pageHome/components/data/ThrillersData.jsx
@@ -0,0 +1,188 @@
+const thrillers = [
+  {
+    //1
+    name: "The Wicked Boy: An Infamous Murder in Victorian London",
+    author: "Kate Summerscale ",
+    descriptionShort:
+      "In the summer of 1895, Robert Coombes (age 13) and his brother Nattie (age 12) were seen spending lavishly around the docklands of East London -- for ten days in July, they ate out at coffee houses and took trips to the seaside and the theater.",
+    description:
+      "The boys told neighbors they had been left home alone while their mother visited family in Liverpool, but their aunt was suspicious. When she eventually forced the brothers to open the house to her, she found the badly decomposed body of their mother in a bedroom upstairs. Robert and Nattie were arrested for matricide and sent for trial at the Old Bailey.",
+    details: {
+      publisher: "Penguin Books",
+      publishDate: "July 04, 2017",
+      pages: "416",
+      dimensions: "5.5 X 8.0 X 0.9 inches | 0.7 pounds",
+      language: "English",
+      type: "Paperback",
+    },
+    pris: "$18.00",
+    image:
+      "https://images-us.bookshop.org/ingram/9780143110460.jpg?height=500&v=v2",
+    aboutTheAuthor:
+      "Kate Summerscale, formerly the literary editor of the Daily Telegraph, is the author of The Queen of Whale Cay, which won a Somerset Maugham Award and was short-listed for the Whitbread Biography Prize. The Suspicions of Mr. Whicher was a #1 bestseller in the UK, has been translated into more than a dozen languages, was short-listed for the CWA Gold Dagger for Non-Fiction and the Edgar Award for Best Fact Crime, and won the Samuel Johnson Prize for Non-Fiction and the British Book Awards Book of the Year. Summerscale lives in London.",
+    reviews1:
+      "Kate Summerscale is deft at interweaving weaving her narrative with extensive quotes from court proceedings and press accounts. Don't look to The Wicked Boy for either amped-up emotion or for sanitization of the facts. It reads like the successful and well-balanced offspring of a liaison between a crime novel and a scholarly paper.",
+    authorReviews1: "-- Florida Times Union",
+    reviews2:
+      "A remarkable job of historical reconstruction.... In the time-honored tradition of Victorian crime stories, The Wicked Boy is a compelling mixture of the gruesome and the perfectly ordinary, a brew uniquely British.... a feat of genuine detective work.",
+    authorReviews2: "--Dallas Morning News",
+    reviews3: "Narrative nonfiction that reads like a novel.",
+    authorReviews3: "-- Omnivoracious Best History Books of July",
+  },
+
+  {
+    //2
+    name: "Tell Me Everything: The Story of a Private Investigation",
+    author: "Erika Krouse",
+    descriptionShort:
+      "Part memoir and part literary true crime, Tell Me Everything is the mesmerizing story of a landmark sexual assault investigation and the female private investigator who helped crack it open.",
+    description:
+      "Erika Krouse has one of those faces. I don't know why I'm telling you this, people say, spilling confessions. In fall 2002, Erika accepts a new contract job investigating lawsuits as a private investigator. The role seems perfect for her, but she quickly realizes she has no idea what she's doing. Then a lawyer named Grayson assigns her to investigate a sexual assault, a college student who was attacked by football players and recruits at a party a year earlier. Erika knows she should turn the assignment down. Her own history with sexual violence makes it all too personal. But she takes the job anyway, inspired by Grayson's conviction that he could help change things forever. And maybe she could, too. Over the next five years, Erika learns everything she can about P. I. technique, tracking down witnesses and investigating a culture of sexual assault and harassment ingrained in the university's football program. But as the investigation grows into a national scandal and a historic civil rights case that revolutionizes Title IX law, Erika finds herself increasingly consumed. When the case and her life both implode at the same time, Erika must figure out how to help win the case without losing herself.",
+    details: {
+      publisher: "Flatiron Books",
+      publishDate: "March 15, 2022",
+      pages: "288",
+      dimensions: "6.5 X 9.9 X 1.1 inches | 1.0 pounds",
+      language: "English",
+      type: "Hardcover",
+    },
+    pris: " $26.96",
+    image:
+      "https://images-us.bookshop.org/ingram/9781250240309.jpg?height=500&v=v2",
+    aboutTheAuthor:
+      "Erika Krouse is the author of Come Up and See Me Sometime, a New York Times Notable Book, and Contenders, a finalist for the VCU Cabell First Novelist Award. Erika's fiction has been published in The New Yorker, The Atlantic, Ploughshares, One Story, and more. She teaches creative writing at the Lighthouse Writers Workshop and lives in Colorado. Her debut memoir, Tell Me Everything, has been optioned for TV adaptation by Playground Entertainment.",
+    reviews1: "",
+    authorReviews1: "",
+    reviews2: "",
+    authorReviews2: "",
+    reviews3: "",
+    authorReviews3: "",
+  },
+
+  {
+    //3
+    name: "Barren",
+    author: "Jj Savaunt",
+    descriptionShort:
+      "A troubling series of missing person cases lead back to Mercy Hospital, but the truth is far more terrifying than you could ever imagine...",
+    description:
+      "After receiving a promotion, Dr. Jacqui is thrilled to begin a career helping infertile women conceive. However, her excitement soon turns to fear when she realizes what her patients must give up in exchange.",
+    details: {
+      publisher: "Bloodhound Books",
+      publishDate: "November 15, 2022",
+      pages: "266",
+      dimensions: "5.25 X 8.0 X 0.6 inches | 0.67 pounds",
+      language: "English",
+      type: "Paperback",
+    },
+    pris: "$17.66",
+    image:
+      "https://images-us.bookshop.org/ingram/9781504081818.jpg?height=500&v=v2",
+    aboutTheAuthor:
+      "Discover more of the author’s books, see similar authors, read author blogs and more",
+    reviews1: "",
+    authorReviews1: "",
+    reviews2: "",
+    authorReviews2: "",
+    reviews3: "",
+    authorReviews3: "",
+  },
+
+  {
+    //4
+    name: "The Secret Place",
+    author: "Tana French",
+    descriptionShort:
+      "A year ago a boy was found murdered at a girlsʼ boarding school, and the case was never solved. Detective Stephen Moran has been waiting for his chance to join Dublin's Murder Squad when sixteen-year-old Holly Mackey arrives in his office with a photo of the boy with the caption: I KNOW WHO KILLED HIM.",
+    description:
+      "Stephen joins with Detective Antoinette Conway to reopen the case--beneath the watchful eye of Holly's father, fellow detective Frank Mackey. With the clues leading back to Holly's close-knit group of friends, to their rival clique, and to the tangle of relationships that bound them all to the murdered boy, the private underworld of teenage girls turns out to be more mysterious and more dangerous than the detectives imagined.",
+    details: {
+      publisher: "Penguin Books",
+      publishDate: "August 04, 2015",
+      pages: "480",
+      dimensions: "5.4 X 8.3 X 1.1 inches | 0.85 pounds",
+      language: "English",
+      type: "Paperback",
+    },
+    pris: "$16.74",
+    image:
+      "https://images-us.bookshop.org/ingram/9780143127512.jpg?height=500&v=v2",
+    aboutTheAuthor:
+      "Tana French is also the author of In the Woods, The Likeness, Faithful Place, Broken Harbor and The Secret Place. Her books have won awards including the Edgar, Anthony, Macavity, and Barry awards, the Los Angeles Times Award for Best Mystery/Thriller, and the Irish Book Award for Crime Fiction. She lives in Dublin with her family.",
+    reviews1:
+      "A book full of giddy, slangy, devious schoolgirls who cannot be trusted about anything, at least not on the first, second, third or fourth rounds of questioning...Part of this book's trickiness is its way of letting characters hide the truth behind the smoke screen of language and let both readers and investigators gradually figure out who is lying.",
+    authorReviews1: "-- Janet Maslin, The New York Times",
+    reviews2:
+      "There are echoes of Leopold and Loeb and Donna Tartt's The Secret History, but the language and landscape are unmistakably French's, as is the way she excavates the past to illuminate the present.",
+    authorReviews2: "--O Magazine",
+    reviews3: "Terrific--terrifying, amazing, and the prose is incandescent.",
+    authorReviews3: "--Stephen King",
+  },
+
+  {
+    //5
+    name: "Black Chalk",
+    author: "Christopher J Yates",
+    descriptionShort:
+      "A compulsively readable psychological thriller set in New York and at Oxford University in which a group of six students play an elaborate game of dares and consequences with tragic result.",
+    description:
+      "It was only ever meant to be a game played by six best friends in their first year at Oxford University; a game of consequences, silly forfeits, and childish dares. But then the game changed: The stakes grew higher and the dares more personal and more humiliating, finally evolving into a vicious struggle with unpredictable and tragic results. Now, fourteen years later, the remaining players must meet again for the final round. Who knows better than your best friends what would break you?",
+    details: {
+      publisher: "Picador USA",
+      publishDate: "August 04, 2015",
+      pages: "368",
+      dimensions: "5.5 X 0.9 X 8.2 inches | 0.6 pounds",
+      language: "English",
+      type: "Paperback",
+    },
+    pris: " $16.74",
+    image:
+      "https://images-us.bookshop.org/ingram/9781250075550.jpg?height=500&v=v2",
+    aboutTheAuthor:
+      "CHRISTOPHER J. YATES was born and raised in Kent and studied law at Oxford University before working as a puzzle editor in London. He now lives in New York City with his wife and dog. Black Chalk is his debut novel.",
+    reviews1:
+      "This is the smart summer thriller you've been waiting for. The black and harmful little book you want in your carry-on.The novel you should be reading tonight.",
+    authorReviews1: "--NPR",
+    reviews2:
+      "One of the greatest surprise reveals I've witnessed. A twist that's like screwing your head on backwards. And when you see the tricky switch Yates has pulled, you just want to kiss him, because dammit if there isn't something sweet and satisfying about a plot twist pulled off with aplomb.",
+    authorReviews2: "--NPR.org",
+    reviews3:
+      "Christopher J. Yates' debut novel is a psychological thriller about the consequences of friendship gone awry.... The result is a story littered with twists that will keep you guessing until the final page.",
+    authorReviews3: "--Paste Magazine",
+  },
+
+  {
+    //6
+    name: "Ninth House",
+    author: "Leigh Bardugo",
+    descriptionShort:
+      "The smash New York Times bestseller from Leigh Bardugo, a mesmerizing tale of power, privilege, and dark magic set among the Ivy League elite.",
+    description:
+      "Galaxy Stern is the most unlikely member of Yale's freshman class. Raised in the Los Angeles hinterlands by a hippie mom, Alex dropped out of school early and into a world of shady drug-dealer boyfriends, dead-end jobs, and much, much worse. In fact, by age twenty, she is the sole survivor of a horrific, unsolved multiple homicide. Some might say she's thrown her life away. But at her hospital bed, Alex is offered a second chance: to attend one of the world's most prestigious universities on a full ride. What's the catch, and why her?",
+    details: {
+      publisher: "FlOctober 08, 2019atiron Books",
+      publishDate: "",
+      pages: "480",
+      dimensions: "6.4 X 9.3 X 1.5 inches | 1.5 pounds",
+      language: "English",
+      type: "Hardcover",
+    },
+    pris: "$26.03",
+    image:
+      "https://images-us.bookshop.org/ingram/9781250313072.jpg?height=500&v=v2",
+    aboutTheAuthor:
+      "Leigh Bardugo is a #1 New York Times bestselling author of fantasy novels and the creator of the Grishaverse (coming soon to Netflix) which spans the Shadow and Bone Trilogy, the Six of Crows Duology, The Language of Thorns, and the King of Scars duology. Her short stories can be found in multiple anthologies, including Best American Science Fiction & Fantasy. Leigh was born in Jerusalem, grew up in Southern California, and graduated from Yale University. These days she lives and writes in Los Angeles.",
+    reviews1:
+      "Ninth House is the best fantasy novel I've read in years, because it's about real people. Bardugo's imaginative reach is brilliant, and this story--full of shocks and twists--is impossible to put down.",
+    authorReviews1: "- Stephen King",
+    reviews2:
+      "inth House is one of the best fantasy novels I've read in years. This book is brilliant, funny, raw and utterly magnificent -- it's a portal to a world you'll never want to leave.",
+    authorReviews2:
+      "- Lev Grossman, New York Times bestselling author of The Magicians",
+    reviews3:
+      "Genuinely terrific...The worldbuilding is rock solid, the plot is propulsive, and readers will be clamoring for a sequel as soon as they read the last page.",
+    authorReviews3: "- Library Journal (starred review)",
+  },
+];
+
+export default thrillers;
