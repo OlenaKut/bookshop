ec381b8aee49a7f0dab4cc63d6c84b79b1b2e3ec
OlenaKut <cundrochka@gmail.com>
2023-04-14 12:13:32 +0200

    Added data poetry

--- src/pages/pageHome/components/data/PoetryData.jsx
+++ src/pages/pageHome/components/data/PoetryData.jsx
@@ -1,21 +1,25 @@
 const poetry = [
   {
     //1
-    name: "",
-    author: "",
-    descriptionShort: "",
-    description: "",
+    name: "On This Side of the Desert",
+    author: "Alfredo Aguilar",
+    descriptionShort:
+      "i say / my mother's name, / cristina & desert marigolds / crack through a boulder. / i say my father's name, martin / & all the novena candles / in the bed of the truck are aglow.",
+    description:
+      "These lines from the book's titular poem On This Side of the Desert encapsulate the dominant themes of the collection: the power and meaning derived from the act of naming; the deep interconnectedness of Latinx cultures, a product of strong family traditions and an intimate relationship with the natural world; and a profound spirituality rooted in the sacraments of Catholic orthodoxy.",
     details: {
-      publisher: "",
-      publishDate: "",
-      pages: "",
-      dimensions: "",
+      publisher: "Kent State University Press",
+      publishDate: "September 08, 2020",
+      pages: "72",
+      dimensions: "5.4 X 8.3 X 0.4 inches | 0.3 pounds",
       language: "English",
-      type: "Hardcover",
+      type: "Paperback",
     },
-    pris: "",
-    image: "",
-    aboutTheAuthor: "",
+    pris: "$17.00",
+    image:
+      "https://images-us.bookshop.org/ingram/9781606354063.jpg?height=500&v=v2",
+    aboutTheAuthor:
+      "Alfredo Aguilar is the son of Mexican immigrants and author of the chapbook What Happens on Earth (BOAAT Press, 2018). Aguilar is a winner of the 92Y's Discovery Poetry Contest and has been awarded fellowships from the MacDowell Colony, the Bread Loaf Writer's Conference, and the Frost Place. His work has appeared in The Shallow Ends, Best New Poets 2017, Adroit Journal, and elsewhere.",
     reviews1: "",
     authorReviews1: "",
     reviews2: "",
@@ -26,27 +30,34 @@ const poetry = [
 
   {
     //2
-    name: "",
-    author: "",
-    descriptionShort: "",
-    description: "",
+    name: "A History of My Brief Body",
+    author: "Billy-Ray Belcourt",
+    descriptionShort:
+      "The youngest ever winner of the Griffin Prize mines his personal history in a brilliant new essay collection seeking to reconcile the world he was born into with the world that could be.",
+    description:
+      "For readers of Ocean Vuong and Maggie Nelson and fans of Heart Berries by Terese Marie Mailhot, A History of My Brief Body is a brave, raw, and fiercely intelligent collection of essays and vignettes on grief, colonial violence, joy, love, and queerness.",
     details: {
-      publisher: "",
-      publishDate: "",
-      pages: "",
-      dimensions: "",
+      publisher: "Two Dollar Radio",
+      publishDate: "July 14, 2020",
+      pages: "142",
+      dimensions: "5.4 X 7.4 X 0.7 inches | 0.45 pounds",
       language: "English",
-      type: "Hardcover",
+      type: "Paperback",
     },
-    pris: "",
-    image: "",
-    aboutTheAuthor: "",
-    reviews1: "",
-    authorReviews1: "",
-    reviews2: "",
-    authorReviews2: "",
-    reviews3: "",
-    authorReviews3: "",
+    pris: "$14.87",
+    image:
+      "https://images-us.bookshop.org/ingram/9781937512934.jpg?height=500&v=v2",
+    aboutTheAuthor:
+      "BILLY-RAY BELCOURT (he/him) is a writer and academic from the Driftpile Cree Nation. His debut book of poems, This Wound is a World, won the 2018 Griffin Poetry Prize and the 2018 Robert Kroetsch City of Edmonton Book Prize, and was named the Most Significant Book of Poetry in English by an Emerging Indigenous Writer at the 2018 Indigenous Voices Award. It was also a finalist for the Governor General's Literary Award, the Gerald Lampert Memorial Award, and the Raymond Souster Award. It was named by CBC Books as one of the best Canadian poetry collections of the year. Billy-Ray is a Ph.D. student and a 2018 Pierre Elliott Trudeau Foundation Scholar in the Department of English and Film Studies at the University of Alberta. He is also a 2016 Rhodes Scholar and holds a Master's degree in Women's Studies from Wadham College at the University of Oxford.",
+    reviews1:
+      "In his endeavor to honor the reality of Indigenous pain while also remaining steadfastly committed to queer Indigenous joy and utopic futures, Billy-Ray Belcourt has written an incomparable book full of emotion, analysis and poetic beauty.",
+    authorReviews1: "--Kai Minosh Pyle, Star Tribune",
+    reviews2:
+      "A rallying cry for freedom. The ongoingness of resilience demands a manifesto, and here it is: 'Joy is art is an ethics of Resistance.",
+    authorReviews2: "--Kristen Millares Young, The Washington Post",
+    reviews3:
+      "I truly can't stop thinking about this work because it demands more from me as a reader and an artist. It is thunderous in the way that great art cracks open your previously held notions of what was possible.",
+    authorReviews3: "--Sacha Vega, Cha Cha Reader",
   },
 
   {
