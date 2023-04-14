9e765d724a01041e50cfd01d565c0f35ee493c79
OlenaKut <cundrochka@gmail.com>
2023-04-14 11:30:30 +0200

    Added data template

---
+++ src/pages/pageHome/components/data/HorrorData.jsx
@@ -0,0 +1,192 @@
+const horror = [
+  //Horror
+  {
+    //1
+    name: "A Flicker in the Dark",
+    author: "Stacy Willingham",
+    descriptionShort:
+      "When Chloe Davis was twelve, six teenage girls went missing in her small Louisiana town. By the end of the summer, her own father had confessed to the crimes and was put away for life, leaving Chloe and the rest of her family to grapple with the truth and try to move forward while dealing with the aftermath.",
+    description:
+      "Now twenty years later, Chloe is a psychologist in Baton Rouge and getting ready for her wedding. While she finally has a fragile grasp on the happiness she's worked so hard to achieve, she sometimes feels as out of control of her own life as the troubled teens who are her patients. So when a local teenage girl goes missing, and then another, that terrifying summer comes crashing back. Is she paranoid, seeing parallels from her past that aren't actually there, or for the second time in her life, is Chloe about to unmask a killer?",
+    details: {
+      publisher: "Minotaur Books",
+      publishDate: "January 11, 2022",
+      pages: "368",
+      dimensions: "6.3 X 9.2 X 1.5 inches | 1.3 pounds",
+      language: "English",
+      type: "Hardcover",
+    },
+    pris: "$26.03",
+    image:
+      "https://images-us.bookshop.org/ingram/9781250803825.jpg?height=500&v=v2-d5f50e9b40af1115f52addfae6efa399",
+    aboutTheAuthor:
+      "STACY WILLINGHAM worked as a copywriter and brand strategist for various marketing agencies before deciding to write fiction full time. She earned her BA in Magazine Journalism from the University of Georgia and MFA in Writing from the Savannah College of Art & Design. She currently lives in Charleston, SC with her husband, Britt, and Labradoodle, Mako. A Flicker in the Dark is her first novel.",
+    reviews1: "",
+    authorReviews1: "",
+    reviews2: "",
+    authorReviews2: "",
+    reviews3: "",
+    authorReviews3: "",
+  },
+
+  {
+    //2
+    name: "Then She Was Gone",
+    author: "Lisa Jewell ",
+    descriptionShort:
+      "Ellie Mack was the perfect daughter. She was fifteen, the youngest of three. Beloved by her parents, friends, and teachers, and half of a teenaged golden couple. Ellie was days away from an idyllic post-exams summer vacation, with her whole life ahead of her. And then she was gone.",
+    description:
+      "Now, her mother Laurel Mack is trying to put her life back together. It's been ten years since her daughter disappeared, seven years since her marriage ended, and only months since the last clue in Ellie's case was unearthed. So when she meets an unexpectedly charming man in a café, no one is more surprised than Laurel at how quickly their flirtation develops into something deeper. Before she knows it, she's meeting Floyd's daughters--and his youngest, Poppy, takes Laurel's breath away.",
+    details: {
+      publisher: "Atria Books",
+      publishDate: "November 06, 2018",
+      pages: "384",
+      dimensions: "5.3 X 8.2 X 1.1 inches | 0.65 pounds",
+      language: "English",
+      type: "Paperback",
+    },
+    pris: "$14.88",
+    image:
+      "https://images-us.bookshop.org/ingram/9781501154652.jpg?height=500&v=v2",
+    aboutTheAuthor:
+      "Lisa Jewell is the #1 New York Times bestselling author of nineteen novels, including The Family Upstairs and Then She Was Gone, as well as Invisible Girl and Watching You. Her novels have sold over 10 million copies internationally, and her work has also been translated into twenty-nine languages. Connect with her on Twitter @LisaJewellUK, on Instagram @LisaJewellUK, and on Facebook @LisaJewellOfficial.",
+    reviews1: "Then She Was Gone is a riveting thriller.",
+    authorReviews1: "--PopSugar",
+    reviews2:
+      "Jewell teases out her twisty plot at just the right pace, leaving readers on the edge of their seats. There will surely be comparisons to novels such as Emma Donoghue's Room (2010) as well as all of the Girl thrillers, but Jewell's latest really isn't at all derivative. Her multilayered characters are sheer perfection, and even the most astute thriller reader won't see where everything is going until the final threads are unknotted. Those few who do guess early won't mind, as the pace and prose will keep them hooked.",
+    authorReviews2: "-- Booklist (starred review)",
+    reviews3:
+      "For thriller readers, Jewell's latest will not disappoint. Sharply written with twists and turns, it will please fans of Gone Girl, The Girl on the Train, or Luckiest Girl Alive.",
+    authorReviews3: "Library Journal",
+  },
+
+  {
+    //3
+    name: "The Silent Patient",
+    author: "Alex Michaelides",
+    descriptionShort:
+      "The Silent Patient is a shocking psychological thriller of a woman's act of violence against her husband--and of the therapist obsessed with uncovering her motive.",
+    description:
+      "Alicia Berenson's life is seemingly perfect. A famous painter married to an in-demand fashion photographer, she lives in a grand house with big windows overlooking a park in one of London's most desirable areas. One evening her husband Gabriel returns home late from a fashion shoot, and Alicia shoots him five times in the face, and then never speaks another word.",
+    details: {
+      publisher: "Celadon Books",
+      publishDate: "May 04, 2021",
+      pages: "368",
+      dimensions: "5.3 X 8.1 X 1.0 inches | 0.76 pounds",
+      language: "English",
+      type: "Paperback",
+    },
+    pris: " $16.73",
+    image:
+      "https://images-us.bookshop.org/ingram/9781250301703.jpg?height=500&v=v2",
+    aboutTheAuthor:
+      "Alex Michaelides was born and raised in Cyprus. He has an M.A. in English Literature from Trinity College, Cambridge University, and an M.A. in Screenwriting from the American Film Institute in Los Angeles. The Silent Patient was his first novel and debuted at #1 on the New York Times bestseller list. It spent more than a year on the New York Times bestseller list and is sold in a record-breaking 50 countries. His second novel, The Maidens, was an instant New York Times bestseller and garnered rave reviews from The Guardian, Esquire, Entertainment Weekly, and more. After living in London for many years, Alex has recently moved back to his birthplace, Cyprus.",
+    reviews1:
+      "Superb... This edgy, intricately plotted psychological thriller establishes Michaelides as a major player in the field.",
+    authorReviews1: "--Publisher's Weekly, starred review",
+    reviews2:
+      "Pulling off a novel where the protagonist stays mum isn't easy, but this impressive, immersive debut--Brad Pitt's company has snapped up film rights--establishes Michaelides as a writer to watch.",
+    authorReviews2: "--People, Book of the Week",
+    reviews3:
+      "The Silent Patient sneaks up on you like a slash of intimidating shadow on a badly lit street. Alex Michaelides has crafted a totally original, spellbinding psychological mystery so quirky, so unique that it should have its own genre. I read it in two nights and savored every luscious word, every grim encounter, every startling twist. The pages will burn with the friction from your hands turning them.",
+    authorReviews3: "--David Baldacci",
+  },
+
+  {
+    //4
+    name: "The Last Party",
+    author: "Clare Mackintosh ",
+    descriptionShort:
+      "On New Year's Eve, Rhys Lloyd has a house full of guests. His vacation homes on Mirror Lake are a success, and he's generously invited the village to drink champagne with their wealthy new neighbors.",
+    description:
+      "But by midnight, Rhys will be floating dead in the freezing waters of the lake. On New Year's Day, Ffion Morgan has a village full of suspects. The tiny community is her home, so the suspects are her neighbors, friends and family--and Ffion has her own secrets to protect. With a lie uncovered at every turn, soon the question isn't who wanted Rhys dead...but who finally killed him.",
+    details: {
+      publisher: "Sourcebooks Landmark",
+      publishDate: "November 08, 2022",
+      pages: "432",
+      dimensions: "6.34 X 9.2 X 1.39 inches | 1.31 pounds",
+      language: "English",
+      type: "Hardcover",
+    },
+    pris: "$26.03",
+    image:
+      "https://images-us.bookshop.org/ingram/9781728250960.jpg?height=500&v=v2-3e60e0707353dec5eb2cc2db4a9ae76f",
+    aboutTheAuthor:
+      "Clare Mackintosh spent twelve years in the British police, in the criminal investigation department and as a public order commander. She resigned in 2011 to start working as a freelance journalist and social media consultant. She is also the founder of the Chipping Norton Literary Festival.",
+    reviews1:
+      "Detectives Leo and Ffion make a storming debut in this twisty, cleverly-layered tale of worlds colliding.",
+    authorReviews1: "-- Belinda Bauer, author of Snap",
+    reviews2:
+      "Her best yet. A picturesque setting, wonderful cast and a pair of detectives you'll love. Plus I guarantee you'll never guess whodunnit.",
+    authorReviews2: "-- Mark Edwards, author of Here to Stay",
+    reviews3:
+      "Expertly plotted and relentlessly gripping, a perfectly executed murder mystery. The whip-sharp and deeply likeable Ffion leads the investigation, introducing us to a cast of brilliantly realized suspects. I loved every page. ",
+    authorReviews3: "-- Lucy Clarke, author of One of the Girls",
+  },
+
+  {
+    //5
+    name: "The Family Game",
+    author: "Catherine Steadman",
+    descriptionShort:
+      "Harriet Reed, a novelist on the brink of literary stardom, is newly engaged to Edward Holbeck, the heir of an extremely powerful family. And even though Edward has long tried to severe ties with them, news of the couple's marital bliss has the Holbecks inching back into their lives.",
+    description:
+      "As Harriet is drawn into their lavish world, the family seems perfectly welcoming. So when Edward's father, Robert, hands Harriet a tape of a book he's been working on, she is desperate to listen.",
+    details: {
+      publisher: "Ballantine Books",
+      publishDate: "October 18, 2022",
+      pages: "336",
+      dimensions: "6.37 X 9.45 X 1.22 inches | 1.2 pounds",
+      language: "English",
+      type: "Hardcover",
+    },
+    pris: "$26.96",
+    image:
+      "https://images-us.bookshop.org/ingram/9780593158067.jpg?height=500&v=v2-da2d421c7d84d1d443069ee0f08d62bd",
+    aboutTheAuthor:
+      "Catherine Steadman is an actress and author based in London. She has appeared in leading roles on British and American television as well as on stage in the West End, where she has been nominated for a Laurence Olivier Award. She grew up in the New Forest, Hampshire, and now lives in North London with her husband and daughter. Steadman's first novel, Something in the Water, was a New York Times bestseller with rights sold in more than thirty territories. She is also the author of Mr. Nobody and The Disappearing Act.",
+    reviews1:
+      "Instantly compelling and filled with ingenious twists and brilliant characters . . . This is sublime thriller writing.",
+    authorReviews1: "--B. P. Walter, author of The Couple's Secret",
+    reviews2: "Stylish, riveting, hugely atmospheric.",
+    authorReviews2:
+      "--Lucy Foley, New York Times bestselling author of The Guest List",
+    reviews3:
+      "[A] tricky psychological puzzle . . . It's a joy to encounter a suspenseful book whose turns lurk, rather than lumber, around the corner.",
+    authorReviews3: "--The New York Times Book Review",
+  },
+
+  {
+    //6
+    name: "The Turn of the Key",
+    author: "Ruth Ware",
+    descriptionShort:
+      "When she stumbles across the ad, she's looking for something else completely. But it seems like too good an opportunity to miss--a live-in nannying post, with a staggeringly generous salary. And when Rowan Caine arrives at Heatherbrae House, she is smitten--by the luxurious smart home fitted out with all modern conveniences, by the beautiful Scottish Highlands, and by this picture-perfect family.",
+    description:
+      "Writing to her lawyer from prison, she struggles to explain the events that led to her incarceration. It wasn't just the constant surveillance from the home's cameras, or the malfunctioning technology that woke the household with booming music, or turned the lights off at the worst possible time. It wasn't just the girls, who turned out to be a far cry from the immaculately behaved model children she met at her interview. It wasn't even the way she was left alone for weeks at a time, with no adults around apart from the enigmatic handyman.",
+    details: {
+      publisher: "Gallery/Scout Press",
+      publishDate: "May 12, 2020",
+      pages: "384",
+      dimensions: "5.3 X 8.2 X 1.1 inches | 0.6 pounds",
+      language: "English",
+      type: "Paperback",
+    },
+    pris: "$15.80",
+    image:
+      "https://images-us.bookshop.org/ingram/9781501188787.jpg?height=500&v=v2",
+    aboutTheAuthor:
+      "Ruth Ware worked as a waitress, a bookseller, a teacher of English as a foreign language, and a press officer before settling down as a full-time writer. She now lives with her family in Sussex, on the south coast of England. She is the #1 New York Times and Globe and Mail (Toronto) bestselling author of In a Dark, Dark Wood; The Woman in Cabin 10; The Lying Game; The Death of Mrs. Westaway; The Turn of the Key; One by One; The It Girl; and Zero Days. Visit her at RuthWare.com or follow her on Twitter @RuthWareWriter.",
+    reviews1:
+      "Ruth Ware-one of our favorite thriller writers--is bringing down the house... Read it for a fast-paced ride.",
+    authorReviews1: "--theSkimm",
+    reviews2:
+      "Let's just say that if you've got an Echo, you're going to unplug it as soon as you finish the book... What Ware does beautifully is infuse The Turn of the Key with a creepy Gothic sensibility. For all of the novel's contemporary touches--particularly the house's malevolent smart technology--she has delivered an old-fashioned horror story, peopled by children with 'eyes full of malice, ' a dour housekeeper straight out of Rebecca and an inscrutable handyman.",
+    authorReviews2: "--The New York Times Book Review",
+    reviews3:
+      "A clever and elegant update to James's story... Surveillance and home technology slot easily into the conventions of horror: They bring the sense that your environment is invaded and controlled from afar, and that you are never quite as alone as you might wish... The Turn of the Key, and novels like it, point to a new reality. We are all, constantly, haunted.",
+    authorReviews3: "--NPR",
+  },
+];
+
+export default horror;
