b970d8aec52d13b27d1ac7e36a0ad278d9a0ab43
JuliaSmolianska <i.julia.i1990@gmail.com>
2023-04-25 19:58:11 +0200

    install and added React-router

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,29 @@ import Container from "react-bootstrap/Container";
 import HomePage from "./pages/pageHome/HomePage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./pages/pageHome/components/Header";
+import NavMenu from "./pages/pageHome/components/NavMenu";
+import Row from "react-bootstrap/Row";
+import Subheader from "./pages/pageHome/components/Subheader";
+import Main from "./pages/pageHome/components/Main";
 
 const App = () => {
   return (
-    <Container fluid className="App">
-      <HomePage />
-    </Container>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Container fluid className="App">
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Subheader />} />
+        </Routes>
+        <Row>
+          <NavMenu />
+          <Routes>
+            <Route exact path="/" element={<HomePage />} />
+          </Routes>
+        </Row>
+      </Container>
+    </BrowserRouter>
   );
 };
 
