4343753c750aa31efe5f880da0ab2b68d9bed657
OlenaKut <cundrochka@gmail.com>
2023-04-30 03:23:01 +0200

    ---

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Container fluid className="App">
         <Header />
         <Routes>
