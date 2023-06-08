a08ff7dc8012bd430c34bc4fca7d38cf76c73f2e
Olena Kutasevych <cundrochka@gmail.com>
2023-06-08 14:17:53 +0200

    Resp car

--- src/pages/pageHome/components/BestCar.jsx
+++ src/pages/pageHome/components/BestCar.jsx
@@ -44,23 +44,23 @@ const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
     items: 4,
-    slidesToSlide: 4 // optional, default to 1.
+    slidesToSlide: 4
   },
   tablet: {
     breakpoint: { max: 1024, min: 768 },
     items: 3,
-    slidesToSlide: 3 // optional, default to 1.
+    slidesToSlide: 3 
   },
 
    miniTablet: {
     breakpoint: { max: 768, min: 464 },
     items: 2,
-    slidesToSlide: 2 // optional, default to 1.
+    slidesToSlide: 2 
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
     items: 1,
-    slidesToSlide: 1 // optional, default to 1.
+    slidesToSlide: 1 
   }
 };
   return (
